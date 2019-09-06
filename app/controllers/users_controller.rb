c54b3264282ef4126830fd443a27598f28d2541d
Takuhiro-Yoshida <takuhiro.yoshida.tkn@gmail.com>
2019-09-06 05:16:06 +0000

    add pagination

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,26 +1,32 @@
 class UsersController < ApplicationController
-def show
-@user = User.find(params[:id])
-@microposts = @user.microposts.order(created_at: :desc)
-end
+    before_action :logged_in_user, only: [:index]
+    
+    def index
+        @users = User.paginate(page: params[:page],:per_page => 10)
+    end
+    
+    def show
+        @user = User.find(params[:id])
+        @microposts = @user.microposts.order(created_at: :desc)
+    end
 
-def new
-@user = User.new
-end
+    def new
+       @user = User.new
+    end
 
-def create
-@user = User.new(user_params)
-if @user.save
-flash[:success] = "Welcome to the Sample App!"
-redirect_to @user
-else
-render 'new'
-end
-end
+    def create
+        @user = User.new(user_params)
+        if @user.save
+            flash[:success] = "Welcome to the Sample App!"
+            redirect_to @user
+        else
+            render 'new'
+        end
+    end
 
-private
+    private
 
-def user_params
-params.require(:user).permit(:name, :email, :password, :password_confirmation)
-end
+    def user_params
+        params.require(:user).permit(:name, :email, :password, :password_confirmation)
+    end
 end
